4c866244610e144db7518dcf807335daef56260e
Martynas Sudintas <martynas.sudintas@nfq.lt>
2016-03-07 01:15:51 +0200

    clearing out headers

--- src/Command/MergeRequestCloseCommand.php
+++ src/Command/MergeRequestCloseCommand.php
@@ -1,20 +1,5 @@
 <?php
 
-/**
- * @copyright C UAB NFQ Technologies 2015
- *
- * This Software is the property of NFQ Technologies
- * and is protected by copyright law – it is NOT Freeware.
- *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * Contact UAB NFQ Technologies:
- * E-mail: info@nfq.lt
- * http://www.nfq.lt
- */
-
 namespace Martiis\GitlabCLI\Command;
 
 use Doctrine\Common\Cache\FilesystemCache;
