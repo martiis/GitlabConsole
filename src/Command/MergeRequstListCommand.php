a604713177fb19c9dc0cef1d82c8d1981b3d653b
Martynas Sudintas <martynas.sudintas@nfq.lt>
2016-03-07 00:06:20 +0200

    added merge-request:list command

---
+++ src/Command/MergeRequstListCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @copyright C UAB NFQ Technologies 2015
+ *
+ * This Software is the property of NFQ Technologies
+ * and is protected by copyright law – it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * Contact UAB NFQ Technologies:
+ * E-mail: info@nfq.lt
+ * http://www.nfq.lt
+ */
+
+namespace Martiis\GitlabCLI\Command;
+
+use Doctrine\Common\Cache\FilesystemCache;
+use GuzzleHttp\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MergeRequstListCommand extends AbstractProjectAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('merge-request:list')
+            ->setDescription('Display\'s a list of current merge requests.')
+            ->addOption(
+                'all',
+                'a',
+                InputOption::VALUE_NONE,
+                'Includes closed merge requests.'
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var FilesystemCache $cache */
+        $cache = $this->getBag()->get('cache');
+        if (!$cache->contains($input->getArgument('project'))) {
+            throw new \LogicException('Project namespace not found!');
+        }
+
+        $id = $cache->fetch($input->getArgument('project'));
+        $state = $input->getOption('all') ? 'all' : 'opened';
+        /** @var ClientInterface $client */
+        $client = $this->getBag()->get('guzzle');
+        $response = $client->request(
+            'GET',
+            sprintf('projects/%s/merge_requests', $id),
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ],
+                'query' => array_merge(
+                    $client->getConfig('query'),
+                    [
+                        'state' => $state
+                    ]
+                )
+            ]
+        );
+
+        $rows = [];
+        $encoded = json_decode($response->getBody()->getContents(), true);
+
+        foreach ($encoded as $item) {
+            $rows[] = [
+                $item['iid'],
+                $item['title'],
+                empty($item['author']['name']) ? $item['author']['username'] : $item['author']['name'],
+                $item['source_branch'],
+                $item['target_branch'],
+                date('Y-m-d H:i', strtotime($item['created_at'])),
+                date('Y-m-d H:i', strtotime($item['updated_at'])),
+                $item['merge_status'],
+            ];
+        }
+
+        $this
+            ->getIO($input, $output)
+            ->table(
+                ['#', 'Title', 'Author', 'Source', 'Target', 'Created', 'Updated', 'Merge status'],
+                $rows
+            );
+    }
+}
